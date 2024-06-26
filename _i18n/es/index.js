9d95c273a9bb00543ac816e133b1221083759776
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-06-26 10:55:32 -0500

    + added translations

--- _i18n/es/index.js
+++ _i18n/es/index.js
@@ -1,7 +1,31 @@
-import layout from 'modules/qblog/_i18n/es/layout'
-import sidebar from 'modules/qblog/_i18n/es/sidebar'
-
 export default {
-  layout,
-  sidebar
+  "iblog": {
+    "cms": {
+      "sidebar": {
+        "adminGroup": "Blog",
+        "adminPosts": "Entradas de Blog",
+        "adminCategories": "Categorias de Blog",
+        "adminPostsInternal": "Ver entradas de Blog"
+      },
+      "newPost": "Nueva Entrada",
+      "updatePost": "Actualizar Entrada",
+      "newCategory": "Nueva Categoría",
+      "updateCategory": "Actualizar Categoría",
+      "post": "Entrada",
+      "category": "Categoría",
+      "quickCard": {
+        "lastPosts": "Blog - Ultimos Posts"
+      },
+      "form": {
+        "showInMenu": "Mostrar en menu",
+        "featured": "Destacado",
+        "sortOrder": "Orden de prioridad",
+        "availableDate": "Disponible desde"
+      },
+      "documentation": {
+        "posts": "Los artículos son el contenido publicado en las paginas de Blog en tu sitio WEB, estos artículos son importantes para tu sitio por que ayudan a mantener tu sitio en constante actualización. Es posible que los edites agregando imágenes, videos o links externos, al igual que poder modificar el contenido de tus textos.",
+        "categories": "Es posible crear categories en tu Blog, con un nombre, descripción y organizarlas en modo de árbol (categorías padres y categorías hijas) con el fin de categorizar todos tus artículos."
+      }
+    }
+  }
 }
